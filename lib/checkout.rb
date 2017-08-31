99ffa2f3dc6a3981478c0ff4d1cf104f31b0106c
olofvi <olofvi89@gmail.com>
2017-08-31 18:41:23 +0200

    all green for now

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -4,21 +4,28 @@ class Checkout
   attr_accessor :products, :basket, :promotional_disc, :total
 
   def initialize
-    @products=product
+    @products = product
     # @promotional_disc
-    @total=total
-  end
-
-  def scan
-    @products.detect
-    @basket << 
-
-  def total
-    total = @basket.inject(0) { |sum, item| sum + item[:price] }
-    end
+    @total = total
+    @basket = []
   end
 
   def product
     YAML.load_file('./lib/products.yml')
+  end
+
+  def item_code
+    product.select { |obj| obj[:item][:code] }
+  end
+
+  def scan(item_code)
+    @basket.push(item_code)
+  end
+
+  def total_cost
+    @cart.each do |code|
+      product = product.detect { |obj| obj[:product][:code] == code }
+      @total_value += product[:product][:price]
+    end
   end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -3,8 +3,8 @@ require './lib/checkout.rb'
   describe Checkout do
 
     it 'has products on initialize' do
-      expected_output = {1=>['Lavender heart' => 9.53],2=>['Personalised cufflinks'=> 45.00],3=>['Kids T-shirt'=> 19.95]}
-      expect(subject.products).to eq expected_output
+      expected_output = [{:item=>{:name=>"Lavender heart", :code=>1, :price=>9.25}}, {:item=>{:title=>"Personalised cufflinks", :code=>2, :price=>45.0}}, {:item=>{:title=>"Kids T-shirt", :code=>3, :price=>19.95}}]
+      expect(subject.product).to eq expected_output
     end
 
     it 'basket should be empty on initialize' do
@@ -12,10 +12,10 @@ require './lib/checkout.rb'
       expect(subject.basket).to eq expected_output
     end
 
-    it 'it should return total value of objects in basket' do
-      expected_output= 54.53
-      subject.scan[1]
-      subject.scan[2]
+    it 'it should return total of products in basket' do
+      expected_output= [1,2]
+      subject.scan(1)
+      subject.scan(2)
       expect(subject.basket).to eq expected_output
     end
   end
