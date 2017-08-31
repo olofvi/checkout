579672b8f16db0ef48c8ab62a898eacf9c9c453a
olofvi <olofvi89@gmail.com>
2017-08-31 12:42:44 +0200

    it went green

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -6,4 +6,9 @@ require './lib/checkout.rb'
       expected_output = {1=>['Lavender heart' => 9.53],2=>['Personalised cufflinks'=> 45.00],3=>['Kids T-shirt'=> 19.95]}
       expect(subject.products).to eq expected_output
     end
+
+    it 'basket should be empty on initialize' do
+      expected_output = basket = []
+      expect(subject.basket).to eq expected_output
+    end
   end
